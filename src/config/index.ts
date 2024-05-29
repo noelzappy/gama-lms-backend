2907925281d6492157440a48b5af46cae3711afd
Emmanuel Yeboah <noelzappy@gmail.com>
2024-05-29 23:38:50 +0000

    chore: minor update

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,3 +6,5 @@ export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = proce
 export const { MONGO_URL } = process.env;
 
 export const { MAIL_HOST, MAIL_PORT, MAIL_USER, MAIL_PASSWORD, MAIL_FROM, CLIENT_URL } = process.env;
+
+console.log('NODE_ENV', NODE_ENV, 'PORT', PORT, 'SECRET KEY', SECRET_KEY);
