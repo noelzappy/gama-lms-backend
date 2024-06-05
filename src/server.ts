03a638ddf7eef5db682ec4e8ca403ec948cabff9
Emmanuel Yeboah <noelzappy@gmail.com>
2024-06-05 15:39:02 +0000

    chore: minor update

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,9 @@
 import { App } from '@/app';
 import { ValidateEnv } from '@utils/validateEnv';
 import AppRoutes from './routes';
-import { logger } from './utils/logger';
 
 // ValidateEnv();
-try {
-
 
 const app = new App(AppRoutes);
 
 app.listen();
-
-
-} catch (error) {
-
-  logger.log(error);
-
-}
